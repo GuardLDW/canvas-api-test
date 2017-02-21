f45a74559711f82c5c5ee43ab6b0d4d7b17623ca
GuardLDW <303899225@qq.com>
2017-02-21 11:46:07 +0800

    Finish Homework 2

--- out/main.js
+++ out/main.js
@@ -3,98 +3,6 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-var math;
-(function (math) {
-    var Point = (function () {
-        function Point(x, y) {
-            this.x = x;
-            this.y = y;
-        }
-        return Point;
-    }());
-    math.Point = Point;
-    function pointAppendMatrix(point, m) {
-        var x = m.a * point.x + m.c * point.y + m.tx;
-        var y = m.b * point.x + m.d * point.y + m.ty;
-        return new Point(x, y);
-    }
-    math.pointAppendMatrix = pointAppendMatrix;
-    /**
-     * 使用伴随矩阵法求逆矩阵
-     * http://wenku.baidu.com/view/b0a9fed8ce2f0066f53322a9
-     */
-    function invertMatrix(m) {
-        var a = m.a;
-        var b = m.b;
-        var c = m.c;
-        var d = m.d;
-        var tx = m.tx;
-        var ty = m.ty;
-        var determinant = a * d - b * c;
-        var result = new Matrix(1, 0, 0, 1, 0, 0);
-        if (determinant == 0) {
-            throw new Error("no invert");
-        }
-        determinant = 1 / determinant;
-        var k = result.a = d * determinant;
-        b = result.b = -b * determinant;
-        c = result.c = -c * determinant;
-        d = result.d = a * determinant;
-        result.tx = -(k * tx + c * ty);
-        result.ty = -(b * tx + d * ty);
-        return result;
-    }
-    math.invertMatrix = invertMatrix;
-    function matrixAppendMatrix(m1, m2) {
-        var result = new Matrix();
-        result.a = m1.a * m2.a + m1.b * m2.c;
-        result.b = m1.a * m2.b + m1.b * m2.d;
-        result.c = m2.a * m1.c + m2.c * m1.d;
-        result.d = m2.b * m1.c + m1.d * m2.d;
-        result.tx = m2.a * m1.tx + m2.c * m1.ty + m2.tx;
-        result.ty = m2.b * m1.tx + m2.d * m1.ty + m2.ty;
-        return result;
-    }
-    math.matrixAppendMatrix = matrixAppendMatrix;
-    var PI = Math.PI;
-    var HalfPI = PI / 2;
-    var PacPI = PI + HalfPI;
-    var TwoPI = PI * 2;
-    var DEG_TO_RAD = Math.PI / 180;
-    var Matrix = (function () {
-        function Matrix(a, b, c, d, tx, ty) {
-            if (a === void 0) { a = 1; }
-            if (b === void 0) { b = 0; }
-            if (c === void 0) { c = 0; }
-            if (d === void 0) { d = 1; }
-            if (tx === void 0) { tx = 0; }
-            if (ty === void 0) { ty = 0; }
-            this.a = a;
-            this.b = b;
-            this.c = c;
-            this.d = d;
-            this.tx = tx;
-            this.ty = ty;
-        }
-        Matrix.prototype.toString = function () {
-            return "(a=" + this.a + ", b=" + this.b + ", c=" + this.c + ", d=" + this.d + ", tx=" + this.tx + ", ty=" + this.ty + ")";
-        };
-        Matrix.prototype.updateFromDisplayObject = function (x, y, scaleX, scaleY, rotation) {
-            this.tx = x;
-            this.ty = y;
-            var skewX, skewY;
-            skewX = skewY = rotation / 180 * Math.PI;
-            var u = Math.cos(skewX);
-            var v = Math.sin(skewX);
-            this.a = Math.cos(skewY) * scaleX;
-            this.b = Math.sin(skewY) * scaleX;
-            this.c = -v * scaleY;
-            this.d = u * scaleY;
-        };
-        return Matrix;
-    }());
-    math.Matrix = Matrix;
-})(math || (math = {}));
 window.onload = function () {
     //1.任何一个显示对象需要一个1矩阵
     //2.把显示对象的属性转化为自己的相对矩阵
@@ -103,8 +11,11 @@ window.onload = function () {
     var canvas = document.getElementById("app"); //使用 id 来寻找 canvas 元素
     var context2D = canvas.getContext("2d"); //得到内建的 HTML5 对象，拥有多种绘制路径、矩形、圆形、字符以及添加图像的方法
     var stage = new DisplayObjectContainer();
-    stage.alpha = 0.5;
-    stage.x = 90;
+    //第二层容器
+    var panel = new DisplayObjectContainer();
+    panel.x = 120;
+    panel.y = 50;
+    panel.alpha = 0.5;
     setInterval(function () {
         context2D.clearRect(0, 0, canvas.width, canvas.height); //在显示图片之前先清屏，将之前帧的图片去掉,清屏范围最好设置成画布的宽与高
         stage.draw(context2D);
@@ -119,7 +30,11 @@ window.onload = function () {
     word1.text = "欧尼酱";
     word1.color = "#FF0000";
     word1.size = 20;
-    word1.alpha = 0.4;
+    word1.alpha = 0.8;
+    var word2 = new TextField();
+    word2.text = "第二层容器";
+    word2.color = "#0000FF";
+    word2.size = 30;
     //图片
     var avater = new Bitmap();
     avater.image.src = "avater.jpg";
@@ -127,9 +42,12 @@ window.onload = function () {
     avater.image.onload = function () {
         avater.width = 400;
         avater.height = 400;
+        //avater.rotation = 25;
+        panel.addChild(word2);
         stage.addChild(avater);
         stage.addChild(word1);
-        //stage.removeChild(word1);
+        stage.addChild(panel);
+        //stage.removeChild(panel);
     };
 };
 var DisplayObject = (function () {
@@ -150,6 +68,7 @@ var DisplayObject = (function () {
     //final，所有子类都要执行且不能修改
     DisplayObject.prototype.draw = function (context2D) {
         this.matrix.updateFromDisplayObject(this.x, this.y, this.scaleX, this.scaleY, this.rotation); //初始化矩阵
+        //console.log(this.matrix.toString());
         //Alpha值
         if (this.parent) {
             this.globalAlpha = this.parent.globalAlpha * this.alpha;
@@ -162,7 +81,6 @@ var DisplayObject = (function () {
         context2D.globalAlpha = this.globalAlpha;
         //console.log(context2D.globalAlpha);
         //变换
-        //console.log(this.matrix.toString());
         context2D.setTransform(this.globalMatrix.a, this.globalMatrix.b, this.globalMatrix.c, this.globalMatrix.d, this.globalMatrix.tx, this.globalMatrix.ty);
         this.render(context2D);
         //模板方法模式
@@ -182,6 +100,7 @@ var Bitmap = (function (_super) {
     }
     Bitmap.prototype.render = function (context2D) {
         context2D.drawImage(this.image, 0, 0, this.width, this.height);
+        //context2D.drawImage(this.image, 0, 0);
     };
     return Bitmap;
 }(DisplayObject));
@@ -217,9 +136,13 @@ var DisplayObjectContainer = (function (_super) {
         displayObject.parent = this;
     };
     DisplayObjectContainer.prototype.removeChild = function (displayObject) {
-        for (var i = 0; i < this.array.length; i++) {
-            if (displayObject == this.array[i]) {
-                this.array.splice(i);
+        var tempArray = this.array.concat();
+        for (var _i = 0, tempArray_1 = tempArray; _i < tempArray_1.length; _i++) {
+            var each = tempArray_1[_i];
+            if (each == displayObject) {
+                var index = this.array.indexOf(each);
+                tempArray.splice(index, 1);
+                this.array = tempArray;
                 return;
             }
         }
--- src/main.ts
+++ src/main.ts
@@ -1,117 +1,3 @@
-module math {
-
-
-    export class Point {
-        x: number;
-        y: number;
-        constructor(x: number, y: number) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    export function pointAppendMatrix(point: Point, m: Matrix): Point {
-        var x = m.a * point.x + m.c * point.y + m.tx;
-        var y = m.b * point.x + m.d * point.y + m.ty;
-        return new Point(x, y);
-
-    }
-
-    /**
-     * 使用伴随矩阵法求逆矩阵
-     * http://wenku.baidu.com/view/b0a9fed8ce2f0066f53322a9
-     */
-    export function invertMatrix(m: Matrix): Matrix {
-
-
-        var a = m.a;
-        var b = m.b;
-        var c = m.c;
-        var d = m.d;
-        var tx = m.tx;
-        var ty = m.ty;
-
-        var determinant = a * d - b * c;
-        var result = new Matrix(1, 0, 0, 1, 0, 0);
-        if (determinant == 0) {
-            throw new Error("no invert");
-        }
-
-        determinant = 1 / determinant;
-        var k = result.a = d * determinant;
-        b = result.b = -b * determinant;
-        c = result.c = -c * determinant;
-        d = result.d = a * determinant;
-        result.tx = -(k * tx + c * ty);
-        result.ty = -(b * tx + d * ty);
-        return result;
-
-    }
-
-    export function matrixAppendMatrix(m1: Matrix, m2: Matrix): Matrix {
-
-        var result = new Matrix();
-        result.a = m1.a * m2.a + m1.b * m2.c;
-        result.b = m1.a * m2.b + m1.b * m2.d;
-        result.c = m2.a * m1.c + m2.c * m1.d;
-        result.d = m2.b * m1.c + m1.d * m2.d;
-        result.tx = m2.a * m1.tx + m2.c * m1.ty + m2.tx;
-        result.ty = m2.b * m1.tx + m2.d * m1.ty + m2.ty;
-        return result;
-    }
-
-    var PI = Math.PI;
-    var HalfPI = PI / 2;
-    var PacPI = PI + HalfPI;
-    var TwoPI = PI * 2;
-    var DEG_TO_RAD: number = Math.PI / 180;
-
-
-    export class Matrix {
-
-        constructor(a: number = 1, b: number = 0, c: number = 0, d: number = 1, tx: number = 0, ty: number = 0) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-            this.d = d;
-            this.tx = tx;
-            this.ty = ty;
-        }
-
-        public a: number;
-
-        public b: number;
-
-        public c: number;
-
-        public d: number;
-
-        public tx: number;
-
-        public ty: number;
-
-        public toString(): string {
-            return "(a=" + this.a + ", b=" + this.b + ", c=" + this.c + ", d=" + this.d + ", tx=" + this.tx + ", ty=" + this.ty + ")";
-        }
-
-        updateFromDisplayObject(x: number, y: number, scaleX: number, scaleY: number, rotation: number) {
-            this.tx = x;
-            this.ty = y;
-            var skewX, skewY;
-            skewX = skewY = rotation / 180 * Math.PI;
-
-            var u = Math.cos(skewX);
-            var v = Math.sin(skewX);
-            this.a = Math.cos(skewY) * scaleX;
-            this.b = Math.sin(skewY) * scaleX;
-            this.c = -v * scaleY;
-            this.d = u * scaleY;
-
-        }
-    }
-}
-
-
 window.onload = () => {
 
     //1.任何一个显示对象需要一个1矩阵
@@ -124,8 +10,13 @@ window.onload = () => {
 
 
     var stage = new DisplayObjectContainer();
-    stage.alpha = 0.5;
-    stage.x = 90;
+
+    //第二层容器
+    var panel = new DisplayObjectContainer();
+    panel.x = 120;
+    panel.y = 50;
+    panel.alpha = 0.5;
+    
     setInterval(() => {
         
         context2D.clearRect(0, 0, canvas.width, canvas.height);//在显示图片之前先清屏，将之前帧的图片去掉,清屏范围最好设置成画布的宽与高
@@ -145,7 +36,12 @@ window.onload = () => {
     word1.text = "欧尼酱";
     word1.color = "#FF0000"
     word1.size = 20;
-    word1.alpha = 0.4;
+    word1.alpha = 0.8;
+
+    var word2 = new TextField();
+    word2.text = "第二层容器"
+    word2.color = "#0000FF"
+    word2.size = 30;
     
     //图片
     var avater = new Bitmap();
@@ -156,11 +52,16 @@ window.onload = () => {
 
         avater.width = 400;
         avater.height = 400;
+        //avater.rotation = 25;
+
+        panel.addChild(word2);
 
         stage.addChild(avater);
         stage.addChild(word1);
 
-        //stage.removeChild(word1);
+        stage.addChild(panel);
+
+        //stage.removeChild(panel);
         
     }
 
@@ -203,19 +104,19 @@ class DisplayObject implements Drawable{
         
 
         this.matrix.updateFromDisplayObject(this.x, this.y, this.scaleX, this.scaleY, this.rotation);//初始化矩阵
+        //console.log(this.matrix.toString());
+
 
         //Alpha值
         if(this.parent){
 
             this.globalAlpha = this.parent.globalAlpha * this.alpha;
             this.globalMatrix = math.matrixAppendMatrix(this.matrix, this.parent.globalMatrix);
-            //console.log("has");
 
         }else{
 
             this.globalAlpha = this.alpha;
             this.globalMatrix = this.matrix;
-            //console.log("has not");
         }
 
         context2D.globalAlpha = this.globalAlpha;
@@ -223,8 +124,6 @@ class DisplayObject implements Drawable{
         
         //变换
         
-       
-        //console.log(this.matrix.toString());
         context2D.setTransform(this.globalMatrix.a, this.globalMatrix.b, this.globalMatrix.c, this.globalMatrix.d, this.globalMatrix.tx, this.globalMatrix.ty);
         this.render(context2D);
 
@@ -257,6 +156,7 @@ class Bitmap extends DisplayObject{
     render(context2D: CanvasRenderingContext2D) {
         
         context2D.drawImage(this.image, 0, 0, this.width, this.height);
+        //context2D.drawImage(this.image, 0, 0);
     }
 }
 
@@ -301,15 +201,20 @@ class DisplayObjectContainer extends DisplayObject{
 
     removeChild(displayObject : DisplayObject){
 
-        for(var i = 0; i < this.array.length; i++){
+        var tempArray = this.array.concat();
+        for(let each of tempArray){
 
-            if(displayObject == this.array[i]){
+            if(each == displayObject){
 
-                this.array.splice(i);
+                var index = this.array.indexOf(each);
+                tempArray.splice(index, 1);
+                this.array = tempArray;
                 return;
             }
         }
     }
+
+
 }
 
 
